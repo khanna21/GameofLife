2478adbd554e00e96ee4a67698a5b69a3f142ebf
user <khann21>
2021-07-02 18:25:58 +0400

    xcvbn

---
+++ Gyol.js
@@ -0,0 +1,48 @@
+class Gyol {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8;
+        this.multiply = 0
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0;
+        for (var i in gyolArr) {
+            if (this.x == gyolArr[i].x && this.y == gyolArr[i].y) {
+                gyolArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
